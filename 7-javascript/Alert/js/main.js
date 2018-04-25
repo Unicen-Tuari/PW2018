8d096f95cf2f262cfdebb88a8e52797aae57777c
ljromero <romeroluisjavier@gmail.com>
2018-04-25 09:15:53 -0300

    JS and Events

---
+++ 7-javascript/Alert/js/main.js
@@ -0,0 +1,4 @@
+alert("Viva el 1995!!");
+alert("Viva el 1996!!");
+alert("Viva el 1997!!");
+alert("Viva el 1998!!");
---
+++ 7-javascript/Boton/js/main.js
@@ -0,0 +1,11 @@
+let contador = 0;
+
+function saludar(){
+  let img = document.getElementById('2');
+    img.src = "img\\Will_1.bmp";
+
+  if(img.src ==  "img\\Will_1.bmp"){
+      img.src = "img\\Will_5.bmp";
+  }
+
+}
